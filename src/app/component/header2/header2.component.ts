b1bd1a2a30d486049771cb4219ee429674ae3bfb
Dev-Guira <guiras086@gmail.com>
2025-10-18 08:33:32 +0000

    ss

--- src/app/component/header2/header2.component.ts
+++ src/app/component/header2/header2.component.ts
@@ -3,31 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header2',
   templateUrl: './header2.component.html',
-  styleUrls: ['./header2.component.scss']
+  styleUrls: ['./header2.component.scss'],
 })
 export class Header2Component {
+  menuItems = [
+    {
+      label: 'Accueil',
+      url: '/',
+    },
+    {
+      label: 'Tarifs',
+      url: '/tarifs',
+    },
+    {
+      label: 'Contact',
+      url: 'contact',
+    },
+    {
+      label: 'Foire aux question',
+      url: 'faq',
+    },
+    {
+      label: 'Apropos',
+      url: 'apropos',
+    },
+  ];
 
-  menuItems=[
-    {
-      label:'Accueil',
-      url:'/'
-    },
-    {
-      label:'Tarifs',
-      url:'/tarifs'
-    },
-    {
-      label:'Contact',
-      url:'contact'
-    },
-    {
-      label:'Foire aux question',
-      url:'faq'
-    },
-     {
-      label:'Apropos',
-      url:'apropos'
-    }
-  ]
+  mdbCollapse = false;
 
+  toggleCollapse() {
+    this.mdbCollapse = !this.mdbCollapse;
+  }
+
+  gotoEspaceClient() {
+    window.open('https://espace.efere.space', '_blank');
+  }
 }
